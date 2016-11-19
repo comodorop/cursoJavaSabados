e6a4324c9a8e990e992812ded3f6e09041794616
TORRES\PabloJavier <comodoro49@gmail.com>
2016-11-19 17:22:08 -0600

    Combos eventos , radio botosn y check

---
+++ src/Practicas/Modelo/DatosPersonales.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicas.Modelo;
+
+/**
+ *
+ * @author PabloJavier
+ */
+public class DatosPersonales {
+
+    private String nombre;
+    private String apellido;
+    private String direccion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "DatosPersonales{" + "nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion + '}';
+    }
+
+}
